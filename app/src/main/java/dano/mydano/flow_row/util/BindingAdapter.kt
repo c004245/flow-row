f21ec7917984f7d760c2a443a921286f0b27c0ec
hyunwook <c004245@naver.com>
2022-08-20 13:47:46 +0900

    chore: custom recycler item view

---
+++ app/src/main/java/dano/mydano/flow_row/util/BindingAdapter.kt
@@ -0,0 +1,14 @@
+package dano.mydano.flow_row.util
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
+    }
+}
